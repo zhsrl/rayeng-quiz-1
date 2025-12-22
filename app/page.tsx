aaf7fba49a6fe8a44f66039b0a82bce4a52e2833
Zhasaral Almabek <thunderccloud@gmail.com>
2025-12-22 15:42:52 +0500

    yametrika cofigured

--- app/page.tsx
+++ app/page.tsx
@@ -146,12 +146,16 @@ export default function App() {
   // Инициализация Яндекс Метрики
   useEffect(() => {
     if (YANDEX_METRIKA_ID !== 0) {
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        // Код инициализации (function(m,e,t,r,i,k,a)...)
+        (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })
+        (window, document, "script", `https://mc.yandex.ru/metrika/tag.js?id=${YANDEX_METRIKA_ID}`, "ym");
 
+        // Вызов init с параметрами из вашего кода
         window.ym(YANDEX_METRIKA_ID, "init", {
              ssr: true,
              webvisor: true,
